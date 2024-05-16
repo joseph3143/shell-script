1be36376a0a48ec37c84824d236a7b2d42558d98
Joseph Raju <121853912+joseph3143@users.noreply.github.com>
2024-05-16 13:01:42 +0530

    Update 01-hello-world.sh

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,10 +2,10 @@
 
 echo "Hello World, I am Learning Devops with AWS"
 
-echo "Ramesh: Hi Suresh, Good Morning"
-echo "Suresh: Good Morning Ramesh"
-echo "Ramesh: How are you Suresh"
-echo "Suresh: I am good, how are you Ramesh"
+echo "Sundar: Hi Suresh, Good Morning"
+echo "Suresh: Good Morning Sundar"
+echo "Sundar: How are you Suresh"
+echo "Suresh: I am good, how are you Sundar"
 
 
 
